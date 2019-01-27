ce3853a518c99b09816d4aea73721110d4361922
evertonmartiss <evertonmartiss@gmail.com>
2019-01-27 14:57:26 -0200

    teste
    testes

---
+++ delta.py
@@ -0,0 +1,16 @@
+print("Digite 3 números para os lados do triangulo")
+
+
+def delta():
+	return b*b-4*a*c	
+
+a=input("digite o primeiro valor")
+b=input("digite o segundo valor")
+c=input("digite o terceiro valor")
+
+a=int(a)
+b=int(b)
+c=int(c)
+
+print(delta())
+
